9912d49c921755bf13bcafe36a0b8dd3d7dfa20e
Sylvain Boissel <sylvain@ashtree.eu>
2016-02-29 18:26:06 +0100

    listes_communes.py

---
+++ backend/liste_communes.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+# from bs4 import BeautifulSoup
+import json
+
+
+def query_category(category, gcmcontinue=''):
+    api_base = "https://fr.wikipedia.org/w/api.php"
+    full_category = 'Catégorie:' + category
+
+    params = [('format', 'json'),
+              ('action', 'query'),
+              ('prop', 'info|pageprops'),
+              ('generator', 'categorymembers'),
+              ('inprop', 'url'),
+              ('ppprop', 'wikibase_item'),
+              ('gcmlimit', 500),
+              ('gcmtitle', full_category)]
+
+    if gcmcontinue:
+        params.append(('gcmcontinue', gcmcontinue))
+
+    try:
+        response = requests.get(api_base, params=params)
+        return json.loads(response.text)
+    except 'ConnectionError':
+        print('Server unreachable')
+
+
+def extract_communes_data(pages, dep_number):
+    results = []
+    for key, page in pages.items():
+        if 'pageprops' in page:
+            qid = page['pageprops']['wikibase_item']
+        else:
+            qid = ''
+        results.append((page['title'],
+                        dep_number,
+                        page['canonicalurl'],
+                        qid))
+
+    return results
+
+
+def writecsv(text, file):
+    if len(text):
+        with open(file, "w") as f:
+            f.write(text)
+            f.close()
+
+categories = [
+              ('01', "Catégorie:Commune de l'Ain"),
+              ('02', "Catégorie:Commune de l'Aisne"),
+              ('03', "Catégorie:Commune de l'Allier"),
+              ('04', "Catégorie:Commune des Alpes-de-Haute-Provence"),
+              ('05', "Catégorie:Commune des Hautes-Alpes"),
+              ('06', "Catégorie:Commune des Alpes-Maritimes"),
+              ('07', "Catégorie:Commune de l'Ardèche"),
+              ('08', "Catégorie:Commune des Ardennes"),
+              ('09', "Catégorie:Commune de l'Ariège"),
+              ('10', "Catégorie:Commune de l'Aube"),
+              ('11', "Catégorie:Commune de l'Aude"),
+              ('12', "Catégorie:Commune de l'Aveyron"),
+              ('13', "Catégorie:Commune des Bouches-du-Rhône"),
+              ('14', "Catégorie:Commune du Calvados"),
+              ('15', "Catégorie:Commune du Cantal"),
+              ('16', "Catégorie:Commune de la Charente"),
+              ('17', "Catégorie:Commune de la Charente-Maritime"),
+              ('18', "Catégorie:Commune du Cher"),
+              ('19', "Catégorie:Commune de la Corrèze"),
+              ('2A', "Catégorie:Commune de la Corse-du-Sud"),
+              ('2B', "Catégorie:Commune de la Haute-Corse"),
+              ('21', "Catégorie:Commune de la Côte-d'Or"),
+              ('22', "Catégorie:Commune des Côtes-d'Armor"),
+              ('23', "Catégorie:Commune de la Creuse"),
+              ('24', "Catégorie:Commune de la Dordogne"),
+              ('25', "Catégorie:Commune du Doubs"),
+              ('26', "Catégorie:Commune de la Drôme"),
+              ('27', "Catégorie:Commune de l'Eure"),
+              ('28', "Catégorie:Commune d'Eure-et-Loir"),
+              ('29', "Catégorie:Commune du Finistère"),
+              ('30', "Catégorie:Commune du Gard"),
+              ('31', "Catégorie:Commune de la Haute-Garonne"),
+              ('32', "Catégorie:Commune du Gers"),
+              ('33', "Catégorie:Commune de la Gironde"),
+              ('34', "Catégorie:Commune de l'Hérault"),
+              ('35', "Catégorie:Commune d'Ille-et-Vilaine"),
+              ('36', "Catégorie:Commune de l'Indre"),
+              ('37', "Catégorie:Commune d'Indre-et-Loire"),
+              ('38', "Catégorie:Commune de l'Isère"),
+              ('39', "Catégorie:Commune du département du Jura"),
+              ('40', "Catégorie:Commune des Landes"),
+              ('41', "Catégorie:Commune de Loir-et-Cher"),
+              ('42', "Catégorie:Commune de la Loire"),
+              ('43', "Catégorie:Commune de la Haute-Loire"),
+              ('44', "Catégorie:Commune de la Loire-Atlantique"),
+              ('45', "Catégorie:Commune du Loiret"),
+              ('46', "Catégorie:Commune du Lot"),
+              ('47', "Catégorie:Commune de Lot-et-Garonne"),
+              ('48', "Catégorie:Commune de la Lozère"),
+              ('49', "Catégorie:Commune de Maine-et-Loire"),
+              ('50', "Catégorie:Commune de la Manche"),
+              ('51', "Catégorie:Commune de la Marne"),
+              ('52', "Catégorie:Commune de la Haute-Marne"),
+              ('53', "Catégorie:Commune de la Mayenne"),
+              ('54', "Catégorie:Commune de Meurthe-et-Moselle"),
+              ('55', "Catégorie:Commune de la Meuse"),
+              ('56', "Catégorie:Commune du Morbihan"),
+              ('57', "Catégorie:Commune de la Moselle"),
+              ('58', "Catégorie:Commune de la Nièvre"),
+              ('59', "Catégorie:Commune du Nord"),
+              ('60', "Catégorie:Commune de l'Oise"),
+              ('61', "Catégorie:Commune de l'Orne"),
+              ('62', "Catégorie:Commune du Pas-de-Calais"),
+              ('63', "Catégorie:Commune du Puy-de-Dôme"),
+              ('64', "Catégorie:Commune des Pyrénées-Atlantiques"),
+              ('65', "Catégorie:Commune des Hautes-Pyrénées"),
+              ('66', "Catégorie:Commune des Pyrénées-Orientales"),
+              ('67', "Catégorie:Commune du Bas-Rhin"),
+              ('68', "Catégorie:Commune du Haut-Rhin"),
+              ('69', "Catégorie:Commune du Rhône"),
+              ('70', "Catégorie:Commune de la Haute-Saône"),
+              ('71', "Catégorie:Commune de Saône-et-Loire"),
+              ('72', "Catégorie:Commune de la Sarthe"),
+              ('73', "Catégorie:Commune de la Savoie"),
+              ('74', "Catégorie:Commune de la Haute-Savoie"),
+              # Paris n'ayant qu'une communne, le 75 n'est pas listé ici
+              ('76', "Catégorie:Commune de la Seine-Maritime"),
+              ('77', "Catégorie:Commune de Seine-et-Marne"),
+              ('78', "Catégorie:Commune des Yvelines"),
+              ('79', "Catégorie:Commune des Deux-Sèvres"),
+              ('80', "Catégorie:Commune de la Somme"),
+              ('81', "Catégorie:Commune du Tarn"),
+              ('82', "Catégorie:Commune de Tarn-et-Garonne"),
+              ('83', "Catégorie:Commune du Var"),
+              ('84', "Catégorie:Commune de Vaucluse"),
+              ('85', "Catégorie:Commune de la Vendée"),
+              ('86', "Catégorie:Commune de la Vienne"),
+              ('87', "Catégorie:Commune de la Haute-Vienne"),
+              ('88', "Catégorie:Commune du département des Vosges"),
+              ('89', "Catégorie:Commune de l'Yonne"),
+              ('90', "Catégorie:Commune du Territoire de Belfort"),
+              ('91', "Catégorie:Commune de l'Essonne"),
+              ('92', "Catégorie:Commune des Hauts-de-Seine"),
+              ('93', "Catégorie:Commune de la Seine-Saint-Denis"),
+              ('94', "Catégorie:Commune du Val-de-Marne"),
+              ('95', "Catégorie:Commune du Val-d'Oise"),
+              ('971', "Catégorie:Commune de la Guadeloupe‎"),
+              ('972', "Catégorie:Commune de la Martinique‎"),
+              ('973', "Catégorie:Commune de la Guyane‎"),
+              ('974', "Catégorie:Commune de La Réunion‎"),
+              ('976', "Catégorie:Commune de Mayotte")]
+
+communes = [('Paris', '75', 'https://fr.wikipedia.org/wiki/Paris', 'Q90')]
+for c in categories:
+    communes_data = query_category(c[1])
+    if 'query' in communes_data:
+        if 'pages' in communes_data['query']:
+            communes += extract_communes_data(
+                communes_data['query']['pages'],
+                c[0])
+
+    while 'continue' in communes_data:
+        gcmcontinue = communes_data['continue']['gcmcontinue']
+        communes_data = query_category(c[1], gcmcontinue)
+        if 'query' in communes_data:
+            if 'pages' in communes_data['query']:
+                communes += extract_communes_data(
+                    communes_data['query']['pages'],
+                    c[0])
+
+        print('continue for {}'.format(c[0]))
+    else:
+        print('OK for {}'.format(c[0]))
+
+communes = sorted(communes)
+
+output = ""
+counters = {}
+for c in communes:
+    title, dep_number, url, qid = c
+    output += '"{}" ; "{}" ; "{}" ; "{}"\n'.format(title, dep_number, url, qid)
+    if dep_number not in counters:
+        counters[dep_number] = 1
+    else:
+        counters[dep_number] += 1
+
+    if not qid:
+        print("Missing Qid for {} ({}, {})".format(title, dep_number, url))
+
+writecsv(output, 'liste-communes.csv')
---
+++ backend/macommune.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup, NavigableString
+import datetime
+import json
+import os               # Files and folder manipulations
+import re               # Regular expressions
+import csv              # CSV file manipulations
+import sys
+from collections import Counter
+from termcolor import colored
+
+# Constants
+VERBOSE = 1
+LANGUAGE = 'fr'
+WD_BASE_URL = 'https://www.wikidata.org/wiki/'
+WP_BASE_URL = 'https://{}.wikipedia.org/wiki/'.format(LANGUAGE)
+
+"""
+Données à récupérer d'un article
+- Titre
+- id Wikidata
+- Présence d'un fichier de prononciation
+- Remplissage du plan
+- Taille de chaque section (en octet)
+- présence d'un article détaillé pour la section
+- évaluation (depuis la PDD)
+- images
+"""
+
+
+class Article(object):
+    def __init__(self, qid):
+        self.qid = qid
+        # The label in Wikidata for the selected LANGUAGE
+        self.wd_label = ''
+        # The description in Wikidata for the selected LANGUAGE
+        self.wd_description = ''
+        # The aliases in Wikidata for the selected LANGUAGE
+        self.wd_aliases = []
+        self.wp_url = ''                # The full URL to the Wikipedia article
+        self.wp_title = ''
+        self.wp_badges = []
+        self.claims = {}
+
+        self.country = ""
+
+    def getWikidataContent(self):
+        """
+        Retrieves some data about the city from the Wikidata item
+
+        Données à récupérer depuis Wikidata :
+        - P373  Catégorie Commons
+        - P1082 Population 
+        - P18   présence ou non d'une image enregistrée
+        - P17   Pays (pour extension éventuelle du projet à d'autre pays)
+        - P281  Code postal
+        - P374  code commune INSEE
+        - P625  coordonnées géographiques
+        - Département ???
+
+        """
+        wd_url = "{}Special:EntityData/{}.json".format(WD_BASE_URL, self.qid)
+        print("Fetching: {}".format(wd_url))
+        response = requests.get(wd_url)
+        wd_content = json.loads(response.text)
+        item_content = wd_content['entities'][self.qid]
+
+        # Label, description, aliases
+        self.wd_label = item_content['labels'][LANGUAGE]['value']
+        self.wd_description = item_content['descriptions'][LANGUAGE]['value']
+
+        aliases = item_content['aliases'][LANGUAGE]
+        for alias in aliases:
+            self.wd_aliases.append(alias['value'])
+
+        # Link to Wikipedia
+        self.wp_url = item_content['sitelinks'][LANGUAGE + 'wiki']['url']
+        self.wp_title = item_content['sitelinks'][LANGUAGE + 'wiki']['title']
+        self.wp_badges = item_content['sitelinks'][
+            LANGUAGE + 'wiki']['badges']  # list
+
+        # Claims
+        self.claims = item_content['claims']
+        self.getClaimContent('P131')
+
+        print(self.qid, self.wd_label, self.wd_description, self.wd_aliases,
+              self.wp_url, self.wp_title, self.wp_badges)
+
+    def getClaimContent(self, property):
+        """
+        returns an array of the preferred values for the claim.
+        If claims with a "preffered" rank are present, they are returned,
+        else those with a "normal" rank are.
+
+        Claims with an "obsolete" rank, or with an ending date qualifier
+        are ignored.
+        """
+
+        preffered_values = []
+        normal_values = []
+
+        claims = self.claims[property]
+
+        for c in claims:
+            value = c['mainsnak']
+            print(c)
+        # print(claim_content)
+
+######
+
+communes = ['Q90',  # Paris
+            'Q214396',  # Broons
+            ]
+commune = Article('Q214396')
+# "Q214396" = Broons
+commune.getWikidataContent()
