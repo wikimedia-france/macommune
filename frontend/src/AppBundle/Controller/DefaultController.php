f3ae3d92749c9d923fdd93f45a1f496355f49d73
Nico <nico@picapo.net>
2016-04-14 14:18:34 +0200

    * Ajout d’un squelette pour la page d’accueil

--- frontend/src/AppBundle/Controller/DefaultController.php
+++ frontend/src/AppBundle/Controller/DefaultController.php
@@ -8,14 +8,11 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
-    }
+	/**
+	* @Route("/", name="home")
+	*/
+	public function indexAction(Request $request)
+	{
+		return $this->render('index.html.twig', array());
+	}
 }
