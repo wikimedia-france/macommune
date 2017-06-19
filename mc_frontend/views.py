61fb5ab2c8070d4d1c4c8f10b732a447cf638e47
Antoine Lamielle <gh@0x010c.fr>
2017-06-19 11:56:01 +0200

    [api] add the geoshape endpoint

--- mc_frontend/views.py
+++ mc_frontend/views.py
@@ -29,3 +29,41 @@ def autocomplete(request, snak):
     values = {'snak': snak,
               'values': list(data.values('wp_title', 'qid'))}
     return HttpResponse(json.dumps(values), content_type='application/json')
+
+
+def geo_api(request, min_lat, max_lat, min_lng, max_lng):
+    min_lat = float(min_lat)
+    max_lat = float(max_lat)
+    min_lng = float(min_lng)
+    max_lng = float(max_lng)
+    
+    #Check if the area is not to big
+    if (max_lat - min_lat) * (max_lng - min_lng) > 1:
+        values = {
+            'type': 'FeatureCollection',
+            'features': [],
+        }
+        return HttpResponse(json.dumps(values), content_type='application/json')
+    
+    #Fetch the geoshape of all cities in the given area
+    data = Communes.objects.filter(latitude__range=[min_lat, max_lat], longitude__range=[min_lng, max_lng])
+    dataset = data.values('wp_title', 'qid', 'geoshape')
+    
+    #Build the geoJSON response
+    features = []
+    for commune in dataset:
+        features += [{
+            "type": "Feature",
+            "properties": {
+                "title": commune['wp_title'],
+                "qid": commune['qid']
+            },
+            "geometry": json.loads(commune['geoshape']),
+        }]
+    
+    values = {
+        'type': 'FeatureCollection',
+        'features': features,
+    }
+    
+    return HttpResponse(json.dumps(values), content_type='application/json')
