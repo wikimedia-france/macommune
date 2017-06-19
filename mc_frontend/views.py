d201ec172d909f3fa14d03a25ebdacdb24113afb
Sylvain Boissel <sylvain@ashtree.eu>
2017-06-19 19:32:13 +0200

    PEP8

--- mc_frontend/views.py
+++ mc_frontend/views.py
@@ -17,6 +17,7 @@ def entity(request, qid, title):
 
 def item_api(request, qid):
     commune = Article(qid)
+    commune.get_live_wp_data()
     data = commune.get_data()
     return HttpResponse(json.dumps(data), content_type='application/json')
 
@@ -34,25 +35,28 @@ def autocomplete(request, snak):
 def geo_api(request, min_lat, max_lat, min_lng, max_lng):
     SEARCH_MARGIN = 0.06
     MAX_AREA = 0.8
-    
+
     min_lat = float(min_lat) - SEARCH_MARGIN
     max_lat = float(max_lat) + SEARCH_MARGIN
     min_lng = float(min_lng) - SEARCH_MARGIN
     max_lng = float(max_lng) + SEARCH_MARGIN
-    
-    #Check if the area is not to big
+
+    # Check if the area is not to big
     if (max_lat - min_lat) * (max_lng - min_lng) > MAX_AREA:
         values = {
             'type': 'FeatureCollection',
             'features': [],
         }
-        return HttpResponse(json.dumps(values), content_type='application/json')
-    
-    #Fetch the geoshape of all cities in the given area
-    data = Communes.objects.filter(latitude__range=[min_lat, max_lat], longitude__range=[min_lng, max_lng])
+        return HttpResponse(json.dumps(values),
+                            content_type='application/json')
+
+    # Fetch the geoshape of all cities in the given area
+    data = Communes.objects.filter(
+        latitude__range=[min_lat, max_lat],
+        longitude__range=[min_lng, max_lng])
     dataset = data.values('title', 'qid', 'geoshape')
-    
-    #Build the geoJSON response
+
+    # Build the geoJSON response
     features = []
     for commune in dataset:
         features += [{
@@ -63,10 +67,10 @@ def geo_api(request, min_lat, max_lat, min_lng, max_lng):
             },
             "geometry": json.loads(commune['geoshape']),
         }]
-    
+
     values = {
         'type': 'FeatureCollection',
         'features': features,
     }
-    
+
     return HttpResponse(json.dumps(values), content_type='application/json')
