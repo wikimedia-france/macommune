1d62775ef558f5f6c53efe1e319ce9346d8208f0
Ash_Crow <sylvain@ashtree.eu>
2017-07-18 15:58:34 +0200

    [backend] installing django_extensions

--- macommune/settings.py
+++ macommune/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'bootstrap3',
     'mc_frontend',
 ]
