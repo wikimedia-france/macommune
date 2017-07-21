8e9f2b41143e7e094c23e06b93a610d3ab7205f1
Antoine Lamielle <gh@0x010c.fr>
2017-07-21 17:44:55 +0200

    [frontend] map: Add legend on the bottom right

--- mc_frontend/static/mc_frontend/js/map.js
+++ mc_frontend/static/mc_frontend/js/map.js
@@ -41,9 +41,37 @@ macommune.MapSelector = function( nav ) {
         mapSelector.helpControl.update = function ( title ) {
             this._div.innerHTML = '<h4>Aide</h4>' +  ( title ?
                 'Cliquez pour voir <b>' + title + '</b> en détail.' :
-                ( mapSelector.geoJsonGroup.getLayers().length > 0 ? 'Cliquez sur une ville.' : 'Zoomez vers votre ville.' ));
+                ( mapSelector.geoJsonGroup !== undefined && mapSelector.geoJsonGroup.getLayers().length > 0 ? 'Cliquez sur une ville.' : 'Zoomez vers votre ville.' ));
         };
         mapSelector.helpControl.addTo( mapSelector.map );
+        
+        //Create the legend message
+        mapSelector.legendControl = L.control( { position: 'bottomright' } );
+        mapSelector.legendControl.onAdd = function ( map ) {
+            this._div = L.DomUtil.create('div', 'help-control info legend'),
+                grades = [ 0, 20, 40, 60, 80, 100 ],
+                labels = [ 'peu avancé', '', '', '', '', 'très complet' ];
+
+            // loop through our density intervals and generate a label with a colored square for each interval
+            for (var i = 0; i < grades.length; i++) {
+                this._div.innerHTML +=
+                    '<i style="background:rgb(' + mapSelector.pickGradientColor( grades[ i ] ).join(',') + ');"></i> ' +
+                    labels[ i ] + ( grades[ i + 1 ] ? '<br>' : '' );
+            }
+            
+            $( this._div ).hide();
+
+            return this._div;
+        };
+        mapSelector.legendControl.disabled = function ( shouldDisable ) {
+            if ( shouldDisable ) {
+                $( this._div ).hide();
+            }
+            else {
+                $( this._div ).show();
+            }
+        };
+        mapSelector.legendControl.addTo( mapSelector.map );
         
         //Refresh the displayed geoJson polygons each times the map is moved
         mapSelector.map.on( 'moveend', mapSelector.onMapMove );
@@ -75,16 +103,23 @@ macommune.MapSelector = function( nav ) {
                 weight: 6,
             };
         }
-        var w = ( feature.properties.avg / 100 ) * 2 - 1;
-        var w1 = ( w * 1 + 1 ) / 2;
-        var w2 = 1 - w1;
-        var rgb = [ Math.round( (33 * w1) + (244 * w2) ),
-                    Math.round( (150 * w1) + (67 * w2) ),
-                    Math.round( (243 * w1) + (54 * w2) ) ];
+        rgb = mapSelector.pickGradientColor( feature.properties.avg );
+        
         return {
             fillColor: 'rgb(' + rgb[ 0 ] + ', ' + rgb[ 1 ] + ', ' + rgb[ 2 ] + ')',
             color: '#336699',
         };
+    }
+    
+    this.pickGradientColor = function( value ) {
+        const color1 = [ 33, 150, 243 ],
+              color2 = [ 244, 67, 54 ]
+        var w = ( value / 100 ) * 2 - 1,
+            w1 = ( w * 1 + 1 ) / 2,
+            w2 = 1 - w1;
+        return [ Math.round( ( color1[ 0 ] * w1 ) + ( color2[ 0 ] * w2 ) ),
+                    Math.round( ( color1[ 1 ] * w1 ) + ( color2[ 1 ] * w2 ) ),
+                    Math.round( ( color1[ 2 ] * w1 ) + ( color2[ 2 ] * w2 ) ) ];
     }
 
     //Clear properly all the geoJson layers
@@ -108,11 +143,10 @@ macommune.MapSelector = function( nav ) {
                        + '/' + bounds.getWest()
                        + '/' + bounds.getEast() )
             .then( function( data ) {
-                console.log( 'Request #' + requestID + ' recieved (current request is at #' + mapSelector.requestCounter + ')' )
                 if ( requestID === mapSelector.requestCounter ) {
                     mapSelector.selectedLayer = undefined;
                     mapSelector.clearGeoJson();
-                    if ( data.features !== undefined && data.features !== [] ) {
+                    if ( data.features !== undefined && data.features.length > 0 ) {
                         mapSelector.geoJsonGroup = L.geoJSON( data, {
                             onEachFeature: mapSelector.onEachFeature,
                             style: mapSelector.colorizeGeoJson,
@@ -121,10 +155,11 @@ macommune.MapSelector = function( nav ) {
                         if ( mapSelector.selectedLayer !== undefined ) {
                             mapSelector.selectedLayer.bringToFront();
                         }
+                        mapSelector.legendControl.disabled( false );
                     }
-                }
-                else {
-                    console.log( 'Discard old request' )
+                    else {
+                        mapSelector.legendControl.disabled( true );
+                    }
                 }
                 mapSelector.helpControl.update();
                 delete data;
@@ -133,6 +168,7 @@ macommune.MapSelector = function( nav ) {
         else {
             mapSelector.clearGeoJson();
             mapSelector.helpControl.update();
+            mapSelector.legendControl.disabled( true );
         }
     };
     
