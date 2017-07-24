d6cf30461a3409d6d78eec671a86806cb97cd27e
Jean-Fred <JeanFred@users.noreply.github.com>
2017-07-24 16:51:31 +0100

    Serve static assets using Whitenoise
    This is useful for development, when there is no
    web server to serve assets.

--- macommune/settings.py
+++ macommune/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,4 +140,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
